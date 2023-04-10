448de9dd05763de62e3d20445c3aba7e05034eff
scrum-1 <scrum1@mde.tw>
2023-04-10 14:07:53 +0800

    加入 cmsimde 架構, 並且修改標題, 以及新增補課頁面

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲:\xa0 https://github.com/mdewcm2023/site-ryan8970 \xa0 \n 網站:  https://mdewcm2023.github.io/site-ryan8970 \xa0', 'tags': '', 'url': 'About.html'}, {'title': '補課', 'text': 'https://mde.tw/wcm2023 \xa0', 'tags': '', 'url': '補課.html'}]};
